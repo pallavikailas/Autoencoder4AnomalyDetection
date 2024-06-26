43c2de4c25957fb154f0efe8abe290a4eae7f422
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-06-26 16:29:53 +0530

    Update data_creater.py

--- data_creater.py
+++ data_creater.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # File paths
-path_1 = "C:/Users/209500/Desktop/SQLtest/data4.xlsx"  # Correct data points
-path_2 = "C:/Users/209500/Desktop/SQLtest/filtered_data.xlsx"    # Wrong data points
+path_1 = "correct_data.xlsx"  # Correct data points
+path_2 = "wrong_data.xlsx"    # Wrong data points
 
 # Read files
 correct_data = pd.read_excel(path_1)
@@ -12,5 +12,5 @@ wrong_data = pd.read_excel(path_2)
 combined_data = pd.concat([correct_data, wrong_data], axis=0)
 
 # Save to a new Excel file
-combined_path = "C:/Users/209500/Desktop/SQLtest/combined_data.xlsx"
+combined_path = "combined_data.xlsx"
 combined_data.to_excel(combined_path, index=False)
