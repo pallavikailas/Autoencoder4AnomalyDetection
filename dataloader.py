720d7d328c2758f804186b6d25cbb25fb41c36ef
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-06-27 11:26:20 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,25 +1,35 @@
-from torch.utils.data import DataLoader
-from excel_file import ExcelDataset
+import pandas as pd
 import torch
+from torch.utils.data import DataLoader, Dataset
+from excel_file import ExcelDataset
 
 torch.manual_seed(42)
 
-# File paths
-dataset = "combined_data.xlsx"
-dataset = ExcelDataset(dataset)
+def create_dataloaders(correct_data_path, wrong_data_path, batch_size=64):
+    # Read the uploaded Excel files
+    correct_data = pd.read_excel(correct_data_path)
+    wrong_data = pd.read_excel(wrong_data_path)
 
-# Train-Val-Test split
-total_size = len(dataset)
-train_size = int(0.6 * total_size)
-val_size = int(0.2 * total_size)
-test_size = total_size - train_size - val_size
+    # Concatenate the two datasets
+    combined_data = pd.concat([correct_data, wrong_data], axis=0)
 
-# Split sequentially
-train_dataset = torch.utils.data.Subset(dataset, range(0, train_size))
-val_dataset = torch.utils.data.Subset(dataset, range(train_size, train_size + val_size))
-test_dataset = torch.utils.data.Subset(dataset, range(train_size + val_size, total_size))
+    # Create the dataset
+    dataset = ExcelDataset(combined_data)
 
-# DataLoaders
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    # Train-Val-Test split
+    total_size = len(dataset)
+    train_size = int(0.6 * total_size)
+    val_size = int(0.2 * total_size)
+    test_size = total_size - train_size - val_size
+
+    # Split sequentially
+    train_dataset = torch.utils.data.Subset(dataset, range(0, train_size))
+    val_dataset = torch.utils.data.Subset(dataset, range(train_size, train_size + val_size))
+    test_dataset = torch.utils.data.Subset(dataset, range(train_size + val_size, total_size))
+
+    # DataLoaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_dataloader, val_dataloader, test_dataloader
