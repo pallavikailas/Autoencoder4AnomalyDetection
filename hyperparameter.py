aac31db7726028ec75fb84c5d1026e4391ba8092
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-06-27 13:40:13 +0530

    Update hyperparameter.py

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,5 +1,3 @@
-import streamlit
-
 num_epochs = 1000
 learning_rate = 0.001
 batch_size = 64
@@ -7,7 +5,3 @@ multiplier = 1.0
 patience = 10
 p = 0.0001
 latent_size = 16
-
-# File uploaders for user to upload their own data files
-file_1 = st.file_uploader("Choose the first Excel file (Correct data)", type="xlsx")
-file_2 = st.file_uploader("Choose the second Excel file (Wrong data)", type="xlsx")
