33c709fbe373c8accfc0aef23cae120029d5bd30
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-06-27 15:17:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,26 +11,30 @@ def main():
     st.header("Upload Files")
     file_1 = st.file_uploader("Choose the first Excel file (Correct data)", type="xlsx")
     file_2 = st.file_uploader("Choose the second Excel file (Wrong data)", type="xlsx")
-    
+
     if file_1 is not None and file_2 is not None:
-        
-        dataset = create_dataset(file_1, file_2)
-        # Create DataLoaders
-        train_dataloader, val_dataloader, test_dataloader = create_dataloaders(dataset)
+        st.info("Files Uploaded Successfully!")
+        if st.button("Start Training"):
+            dataset = create_dataset(file_1, file_2)
+            # Create DataLoaders
+            train_dataloader, val_dataloader, test_dataloader = create_dataloaders(dataset)
                 
-        # Train autoencoder
-        st.header("Training Autoencoder")
-        st.write("Training in progress...")
-        autoencoder = train_model(train_dataloader, val_dataloader)
-        st.write("Training complete!")
+            # Train autoencoder
+            st.header("Training Autoencoder")
+            st.write("Training in progress...")
+            autoencoder = train_model(train_dataloader, val_dataloader)
+            st.write("Training complete!")
         
-        # Detect anomalies
-        st.write("Detecting Anomalies.....")
-        anomalies_df = detect_anomalies(test_dataloader, autoencoder, dataset)
+            # Detect anomalies
+            st.write("Detecting Anomalies.....")
+            anomalies_df = detect_anomalies(test_dataloader, autoencoder, dataset)
         
-        # Display anomalies
-        st.subheader("Anomalies Detected")
-        st.dataframe(anomalies_df)
+            # Display anomalies
+            st.subheader("Anomalies Detected")
+            st.dataframe(anomalies_df)
+            
+    else:
+    st.warning("Please upload both files to start training.")
 
 if __name__ == "__main__":
     main()
