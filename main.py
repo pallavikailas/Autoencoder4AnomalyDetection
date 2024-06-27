1c39162e7f7cac0bbabd4042879903cc10cc84d1
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-06-27 13:36:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from dataloader import create_dataloaders
 from training import train_model
 from anomaly_detection import detect_anomalies
+from hyperparameters import file1, file2
 
 st.title("Anomaly Detection App")
 
@@ -10,10 +11,6 @@ st.write("""
 # Detecting anomalies in aircraft data
 This app uses an autoencoder to detect anomalies in aircraft data and explains the detected anomalies using LIME.
 """)
-
-# File uploaders for user to upload their own data files
-file_1 = st.file_uploader("Choose the first Excel file (Correct data)", type="xlsx")
-file_2 = st.file_uploader("Choose the second Excel file (Wrong data)", type="xlsx")
 
 if file_1 is not None and file_2 is not None:
     # Create DataLoaders
